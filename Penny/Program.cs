6eeafb64c364a3dd56d9abe9bac9ff3463707c68
Christopher Leighton <christopher7694@gmail.com>
2022-12-08 19:57:19 -0800

    add .editorconfig

--- Penny/Program.cs
+++ Penny/Program.cs
@@ -4,10 +4,10 @@ namespace Penny
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
-            var app = builder.Build();
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            WebApplication app = builder.Build();
 
-            app.MapGet("/", () => "Hello World!");
+            _ = app.MapGet("/", () => "Hello World!");
 
             app.Run();
         }
