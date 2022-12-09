5dc56335f3b2dc0d1dc1f058db32a44f3232d31d
Christopher Leighton <christopher7694@gmail.com>
2022-12-08 23:48:17 -0800

    add serilog

--- Penny/Program.cs
+++ Penny/Program.cs
@@ -1,15 +1,41 @@
-namespace Penny
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-            WebApplication app = builder.Build();
+using Serilog;
 
+namespace Penny;
+
+public class Program
+{
+    public static async Task Main(string[] args)
+    {
+        try
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(basePath: Directory.GetCurrentDirectory())
+            .AddJsonFile(path: "appsettings.json", optional: true)
+            .AddJsonFile(path: $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
+            .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom
+                .Configuration(configuration)
+                .CreateLogger();
+
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            _ = builder.Host.UseSerilog();
+
+            WebApplication app = builder.Build();
             _ = app.MapGet("/", () => "Hello World!");
 
-            app.Run();
+            await app.RunAsync();
+
+            Log.Information("Application finished normally");
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application terminated unexpectedly");
+        }
+        finally
+        {
+            await Log.CloseAndFlushAsync();
         }
     }
 }
