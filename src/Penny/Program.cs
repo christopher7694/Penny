fdd22cbe3bd22e361350792cb44fe72fe43b02b2
Christopher Leighton <christopher7694@gmail.com>
2022-12-10 18:07:36 -0800

    use default config order from WebApplication.CreateBuilder

--- src/Penny/Program.cs
+++ src/Penny/Program.cs
@@ -10,18 +10,13 @@ public class Program
     {
         try
         {
-            IConfigurationRoot configuration = new ConfigurationBuilder()
-            .SetBasePath(basePath: Directory.GetCurrentDirectory())
-            .AddJsonFile(path: "appsettings.json", optional: true)
-            .AddJsonFile(path: $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Production"}.json", optional: true)
-            .Build();
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom
-                .Configuration(configuration)
+                .Configuration(configuration: builder.Configuration)
                 .CreateLogger();
 
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
             _ = builder.Host.UseSerilog();
             _ = builder.Services.AddOpenTelemetryTracing(tpBuilder =>
             {
@@ -34,7 +29,6 @@ public class Program
 
             WebApplication app = builder.Build();
             _ = app.MapGet("/", () => "Hello World!");
-
             await app.RunAsync();
 
             Log.Information("Application finished normally");
